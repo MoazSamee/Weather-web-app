415900a6243cbb8b9b050749498ed459e7569eb9
MoazSamee <moazsamikhokher@gmail.com>
2024-06-02 03:32:22 +0500

    chore: Update .gitignore and package.json, rename index.css and index.html to views folder

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import express from "express";
+import bodyParser from "body-parser";
+import morgan from "morgan";
 import axios from "axios";
 import { fileURLToPath } from 'url';
 import { dirname } from 'path';
+import { API_KEY } from "./secret.js";
 
 
 const __filename = fileURLToPath(import.meta.url);
@@ -9,43 +12,55 @@ const __dirname = dirname(__filename);
 
 const app = express();
 const port = 3000;
-
-
+app.use(express.static("views"));
+//app.use(morgan("dev"));
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
 });
-
-
-app.get("/", (req, res) => {
-    res.sendFile(__dirname + "/public/index.html");
-});
-let cityl
+let city="london",data;
 let api;
 
-app.post("/getTemp", async(req, res) => { 
-
-    city = req.body.city;
-    api="place your api from weatherapi.com";
+app.get("/", async (req, res) => {
+   
+    api=`http://api.weatherapi.com/v1/current.json?key=${API_KEY}&q=${city}&aqi=no`;
 
  try{
-    const data = await axios.get(api);
+     data = await axios.get(api);
     
  }catch(error)
  {
     console.log('error occured: ', error);
  }
 
+ res.render("index.ejs",data)
+   
+  
+});
 
 
-   })
+
+app.post("/", async(req, res) => { 
+
+    city = req.body.city;
+    api=`http://api.weatherapi.com/v1/current.json?key=5dd87de4b1cc41d3a30153118240106&q=${city}&aqi=no`;
+
+ try{
+     data = await axios.get(api);
+    
+ }catch(error)
+ {
+    console.log('error occured: ', error);
+ }
+
+ res.render("index.ejs",data)
+   
+ });
 
 
   
-   function displayValues()
-   {
-    document.getElementById("city").innerHTML = city;
-    document.getElementById("temp").innerHTML = data.current.temp_c;
-    document.getElementById("condition").innerHTML = data.current.condition.text;
-    document.getElementById("icon").src = data.current.condition.icon;
-   }
+
+   
+   
+
